864ed5dffccec3fff732456072d49b8460266fa3
Fadilah Hasan <fadilah.hasan@metraplasa.co.id>
2020-06-21 13:36:02 +0700

    initial project

---
+++ Kitabisa Movie/Kitabisa Movie/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Kitabisa Movie
+//
+//  Created by Fadilah Hasan on 21/06/20.
+//  Copyright © 2020 Fadilah Hasan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
